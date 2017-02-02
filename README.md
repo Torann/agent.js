1ed04b1498971cab74fc8b4d912204d985fdf331
Daniel Stainback <torann@gmail.com>
2017-02-02 15:15:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Agent Information
+
+Easily get information about the browser.
+
+## Available Information:
+
+```js
+window.agentDetails.screen;
+window.agentDetails.viewport;
+window.agentDetails.zoom;
+window.agentDetails.pixelRatio;
+window.agentDetails.browser;
+window.agentDetails.browserVersion;
+window.agentDetails.browserMajorVersion;
+window.agentDetails.mobile;
+window.agentDetails.os;
+window.agentDetails.osVersion;
+window.agentDetails.cookies;
+```
---
+++ agent.js
@@ -0,0 +1,226 @@
+/* ========================================================================
+ * Agent Information
+ * ========================================================================
+ * Copyright 2017 Daniel Stainback
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ======================================================================== */
+
++function (window) { "use strict";
+    {
+        var unknown = '-';
+
+        // Screen
+        var screenSize = '';
+        if (screen && screen.width) {
+            var width = (screen.width) ? screen.width : '';
+            var height = (screen.height) ? screen.height : '';
+
+            screenSize = '' + width + " x " + height;
+        }
+
+        // Viewport
+        var viewportSize = window.innerWidth + 'x' + window.innerHeight;
+
+        // Pixel Ratio
+        var pixelRatio = 1;
+        if (window.screen.systemXDPI !== undefined
+            && window.screen.logicalXDPI !== undefined
+            && window.screen.systemXDPI > window.screen.logicalXDPI
+        ) {
+            pixelRatio = window.screen.systemXDPI / window.screen.logicalXDPI;
+        }
+        else if (window.devicePixelRatio !== undefined) {
+            pixelRatio = window.devicePixelRatio;
+        }
+
+        // Browser zoom level
+        var zoom = unknown;
+        if (document.defaultView && document.documentElement) {
+            var dWidth = parseInt(document.defaultView.getComputedStyle(document.documentElement, null).width,10);
+            var cWidth = document.documentElement.clientWidth;
+
+            zoom = dWidth / cWidth;
+        }
+
+        // Browser
+        var nVer = navigator.appVersion;
+        var nAgt = navigator.userAgent;
+        var browser = navigator.appName;
+        var version = '' + parseFloat(navigator.appVersion);
+        var majorVersion = parseInt(navigator.appVersion, 10);
+        var nameOffset, verOffset, ix;
+
+        // Opera
+        if ((verOffset = nAgt.indexOf('Opera')) != -1) {
+            browser = 'Opera';
+            version = nAgt.substring(verOffset + 6);
+
+            if ((verOffset = nAgt.indexOf('Version')) != -1) {
+                version = nAgt.substring(verOffset + 8);
+            }
+        }
+
+        // Opera Next
+        if ((verOffset = nAgt.indexOf('OPR')) != -1) {
+            browser = 'Opera';
+            version = nAgt.substring(verOffset + 4);
+        }
+
+        // Edge
+        else if ((verOffset = nAgt.indexOf('Edge')) != -1) {
+            browser = 'Microsoft Edge';
+            version = nAgt.substring(verOffset + 5);
+        }
+
+        // MSIE
+        else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
+            browser = 'Microsoft Internet Explorer';
+            version = nAgt.substring(verOffset + 5);
+        }
+
+        // Chrome
+        else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
+            browser = 'Chrome';
+            version = nAgt.substring(verOffset + 7);
+        }
+
+        // Safari
+        else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
+            browser = 'Safari';
+            version = nAgt.substring(verOffset + 7);
+            if ((verOffset = nAgt.indexOf('Version')) != -1) {
+                version = nAgt.substring(verOffset + 8);
+            }
+        }
+
+        // Firefox
+        else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
+            browser = 'Firefox';
+            version = nAgt.substring(verOffset + 8);
+        }
+
+        // MSIE 11+
+        else if (nAgt.indexOf('Trident/') != -1) {
+            browser = 'Microsoft Internet Explorer';
+            version = nAgt.substring(nAgt.indexOf('rv:') + 3);
+        }
+
+        // Other browsers
+        else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
+            browser = nAgt.substring(nameOffset, verOffset);
+            version = nAgt.substring(verOffset + 1);
+
+            if (browser.toLowerCase() == browser.toUpperCase()) {
+                browser = navigator.appName;
+            }
+        }
+
+        // Trim the version string
+        if ((ix = version.indexOf(';')) != -1) version = version.substring(0, ix);
+        if ((ix = version.indexOf(' ')) != -1) version = version.substring(0, ix);
+        if ((ix = version.indexOf(')')) != -1) version = version.substring(0, ix);
+
+        majorVersion = parseInt('' + version, 10);
+        if (isNaN(majorVersion)) {
+            version = '' + parseFloat(navigator.appVersion);
+            majorVersion = parseInt(navigator.appVersion, 10);
+        }
+
+        // Mobile version
+        var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
+
+        // Cookie
+        var cookieEnabled = navigator && navigator.cookieEnabled ? true : false;
+
+        if (typeof navigator.cookieEnabled == 'undefined' && !cookieEnabled) {
+            document.cookie = 'testcookie';
+            cookieEnabled = document.cookie.indexOf('testcookie') != -1;
+        }
+
+        // system
+        var os = unknown;
+        var clientStrings = [
+            {s: 'Windows 10', r: /(Windows 10.0|Windows NT 10.0)/},
+            {s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/},
+            {s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/},
+            {s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/},
+            {s: 'Windows Vista', r: /Windows NT 6.0/},
+            {s: 'Windows Server 2003', r: /Windows NT 5.2/},
+            {s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/},
+            {s: 'Windows 2000', r: /(Windows NT 5.0|Windows 2000)/},
+            {s: 'Windows ME', r: /(Win 9x 4.90|Windows ME)/},
+            {s: 'Windows 98', r: /(Windows 98|Win98)/},
+            {s: 'Windows 95', r: /(Windows 95|Win95|Windows_95)/},
+            {s: 'Windows NT 4.0', r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
+            {s: 'Windows CE', r: /Windows CE/},
+            {s: 'Windows 3.11', r: /Win16/},
+            {s: 'Android', r: /Android/},
+            {s: 'Open BSD', r: /OpenBSD/},
+            {s: 'Sun OS', r: /SunOS/},
+            {s: 'Linux', r: /(Linux|X11)/},
+            {s: 'iOS', r: /(iPhone|iPad|iPod)/},
+            {s: 'Mac OS X', r: /Mac OS X/},
+            {s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
+            {s: 'QNX', r: /QNX/},
+            {s: 'UNIX', r: /UNIX/},
+            {s: 'BeOS', r: /BeOS/},
+            {s: 'OS/2', r: /OS\/2/},
+            {s: 'Search Bot', r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+        ];
+
+        for (var id in clientStrings) {
+            var cs = clientStrings[id];
+            if (cs.r.test(nAgt)) {
+                os = cs.s;
+                break;
+            }
+        }
+
+        var osVersion = unknown;
+
+        if (/Windows/.test(os)) {
+            osVersion = /Windows (.*)/.exec(os)[1];
+            os = 'Windows';
+        }
+
+        switch (os) {
+            case 'Mac OS X':
+                osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'Android':
+                osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'iOS':
+                osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+                osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+                break;
+        }
+    }
+
+    window.agentDetails = {
+        screen: screenSize,
+        viewport: viewportSize,
+        zoom: zoom,
+        pixelRatio: pixelRatio,
+        browser: browser,
+        browserVersion: version,
+        browserMajorVersion: majorVersion,
+        mobile: mobile,
+        os: os,
+        osVersion: osVersion.replace('_', '.'),
+        cookies: cookieEnabled
+    };
+}(window);
